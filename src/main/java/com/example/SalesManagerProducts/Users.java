354b23906f6de4f10a46b22c78303843fc78fe77
IvanovaIveta <73793415+IvanovaIveta@users.noreply.github.com>
2020-12-21 18:51:03 +0200

    Update Users.java

--- src/main/java/com/example/SalesManagerProducts/Users.java
+++ src/main/java/com/example/SalesManagerProducts/Users.java
@@ -1,68 +1,68 @@
-//package com.example.SalesManagerProducts;
-//
-//import org.hibernate.annotations.Cascade;
-//
-//import javax.persistence.*;
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//@Entity
-//@Table(name="users")
-//public class Users {
-//
-//
-//    private Integer id;
-//    private String username;
-//    private String password;
-//    private boolean enabled;
-//
-//    @ManyToMany(cascade= CascadeType.ALL, fetch =FetchType.EAGER )
-//    @JoinTable(
-//            name="users_roles",
-//            joinColumns= @JoinColumn(name="user_id"),
-//            inverseJoinColumns = @JoinColumn(name= "role_id")
-//    )
-//    private Set<Role> roles = new HashSet<>();
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    public Integer getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public boolean isEnabled() {
-//        return enabled;
-//    }
-//
-//    public void setEnabled(boolean enabled) {
-//        this.enabled = enabled;
-//    }
-//
-//    public Set<Role> getRoles() {
-//        return roles;
-//    }
-//
-//    public void setRoles(Set<Role> roles) {
-//        this.roles = roles;
-//    }
-//}
+package com.example.SalesManagerProducts;
+
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    @ManyToMany(cascade= CascadeType.ALL, fetch =FetchType.EAGER )
+    @JoinTable(
+            name="users_roles",
+            joinColumns= @JoinColumn(name="user_id"),
+            inverseJoinColumns = @JoinColumn(name= "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+
+    public Integer getId() {
+        return user_id;
+    }
+
+    public void setId(Integer id) {
+        this.user_id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
