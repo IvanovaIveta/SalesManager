a5d8a91a48d3a5528c8ac3430b604609f502e341
IvanovaIveta <73793415+IvanovaIveta@users.noreply.github.com>
2020-12-21 18:54:52 +0200

    Update UserRepository.java

--- src/main/java/com/example/SalesManagerProducts/UserRepository.java
+++ src/main/java/com/example/SalesManagerProducts/UserRepository.java
@@ -1,12 +1,14 @@
-//package com.example.SalesManagerProducts;
-//
-//import org.hibernate.annotations.Parameter;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.data.repository.query.Param;
-//
-//public interface UserRepository extends CrudRepository<Users, Integer> {
-//
-//    @Query("SELECT u FROM Users u WHERE u.username= :username")
-//    public Users getUserByUsername(@Param("username") String username);
-//}
+package com.example.SalesManagerProducts;
+
+import org.hibernate.annotations.Parameter;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<Users, Integer> {
+
+    @Query("SELECT u FROM Users u WHERE u.username= :username")
+    public Users getUserByUsername(@Param("username") String username);
+}
