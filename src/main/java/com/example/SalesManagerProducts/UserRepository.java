7cfd13be3badbdabed03621e9ae49010ae3bd7f0
IvanovaIveta <73793415+IvanovaIveta@users.noreply.github.com>
2020-12-23 23:20:15 +0200

    Update UserRepository.java

--- src/main/java/com/example/SalesManagerProducts/UserRepository.java
+++ src/main/java/com/example/SalesManagerProducts/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<Users, Integer> {
+public interface UserRepository extends CrudRepository<Users, Integer>, JpaRepository<Users, Integer>{
 
     @Query("SELECT u FROM Users u WHERE u.username= :username")
     public Users getUserByUsername(@Param("username") String username);
