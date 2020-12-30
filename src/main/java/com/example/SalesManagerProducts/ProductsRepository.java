624b6ca800d9d435d9a5d5644ab8c9e453571268
IvanovaIveta <73793415+IvanovaIveta@users.noreply.github.com>
2020-12-30 18:42:22 +0200

    Delete ProductsRepository.java

--- src/main/java/com/example/SalesManagerProducts/ProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SalesManagerProducts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductsRepository extends JpaRepository<Products, Integer> {
-
-}
