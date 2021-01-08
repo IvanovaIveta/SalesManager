8b2b82131a11de7e4c115fba27381b2a4a871010
IvanovaIveta <73793415+IvanovaIveta@users.noreply.github.com>
2021-01-08 04:58:48 +0200

    Update SaleController.java

--- src/main/java/com/example/SalesManagerProducts/controllers/SaleController.java
+++ src/main/java/com/example/SalesManagerProducts/controllers/SaleController.java
@@ -1,13 +1,15 @@
 package com.example.SalesManagerProducts.controllers;
 
+import com.example.SalesManagerProducts.entities.Customers;
 import com.example.SalesManagerProducts.entities.Products;
 import com.example.SalesManagerProducts.entities.Sale;
+import com.example.SalesManagerProducts.persistence.CustomersRepository;
 import com.example.SalesManagerProducts.persistence.ProductsRepository;
 import com.example.SalesManagerProducts.persistence.SalesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,12 +22,26 @@ public class SaleController {
     @Autowired
     private ProductsRepository productRepo;
 
+    @Autowired
+    private CustomersRepository customersRepo;
+
     @GetMapping("/newSale")
     public String showNewSaleForm(Model model){
-        List<Products>listProducts=productRepo.findAll();
+        List<Products> listProducts=productRepo.findAll();
         model.addAttribute("sale", new Sale());
         model.addAttribute("listProducts", listProducts);
+
+        List<Customers> listCustomers=customersRepo.findAll();
+        model.addAttribute("listCustomers", listCustomers);
         return "newSale";
     }
+
+
+    @PostMapping("/saveSale")
+    public String saveProduct(Sale sale){
+        salesRepo.save(sale);
+        return "sales";
+    }
+
 
 }
