b53cb26169b7ff17bfc5d136d495e0c57d17f33d
IvanovaIveta <73793415+IvanovaIveta@users.noreply.github.com>
2021-01-10 20:31:56 +0200

    Update AppController.java

--- src/main/java/com/example/SalesManagerProducts/controllers/AppController.java
+++ src/main/java/com/example/SalesManagerProducts/controllers/AppController.java
@@ -41,19 +41,17 @@ public class AppController {
 
     @RequestMapping("/")
     public String viewHomePage(Model model) throws RoleNotFoundException {
-        Iterable<Products> listProducts= service.listAll();
+        Iterable<Products> listProducts = service.listAll();
         model.addAttribute("listProducts", listProducts);
 
-        List<Customers> listCustomers=customersService.listAll();
+        List<Customers> listCustomers = customersService.listAll();
         model.addAttribute("listCustomers", listCustomers);
 
         //  List<Users> listUsers= usersService.listAll();
 
-        List<Users> listUsers=  usersService.getRepresentatives();
+        List<Users> listUsers = usersService.getRepresentatives();
         model.addAttribute("listUsers", listUsers);
 
-//        List<Users> listUsers=  userService.getRepresentatives();
-//        model.addAttribute("listUsers", listUsers);
 
 //        List<Sale>listSales= salesService.listAll();
 //        model.addAttribute("listSales",listSales);
@@ -62,40 +60,44 @@ public class AppController {
     }
 
     @RequestMapping("/new")
-    public String showNewProductForm(Model model){
-        Products product= new Products();
+    public String showNewProductForm(Model model) {
+        Products product = new Products();
         model.addAttribute("product", product);
         return "new_product";
     }
+
     @RequestMapping("/new_customer")
-    public String showNewCustomerForm(Model model){
-        Customers customer= new Customers();
+    public String showNewCustomerForm(Model model) {
+        Customers customer = new Customers();
         model.addAttribute("customer", customer);
         return "new_customer";
     }
-    @RequestMapping ("/newUserSR")
-    public String showNewUserSRForm(Model model){
-      //  Users user= new Users();
+
+    @RequestMapping("/newUserSR")
+    public String showNewUserSRForm(Model model) {
+        //  Users user= new Users();
         model.addAttribute("user", new Users());
-      //  model.addAttribute("user", user);
+        //  model.addAttribute("user", user);
         //model.addAttribute("roles_id", 2);
 
         return "newUserSR";
     }
 
-    @RequestMapping(value="/save", method = RequestMethod.POST)
-    public String saveProduct(@ModelAttribute("product") Products product){
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Products product) {
 
         service.save(product);
         return "redirect:/";
     }
-    @RequestMapping(value="/save_customer", method = RequestMethod.POST)
-    public String saveCustomer(@ModelAttribute("customer") Customers customers){
+
+    @RequestMapping(value = "/save_customer", method = RequestMethod.POST)
+    public String saveCustomer(@ModelAttribute("customer") Customers customers) {
         customersService.save(customers);
         return "redirect:/";
     }
-    @PostMapping (value="/saveUserSR")
-    public String saveUserSR(@ModelAttribute("user") Users users){
+
+    @PostMapping(value = "/saveUserSR")
+    public String saveUserSR(@ModelAttribute("user") Users users) {
 //        BCryptPasswordEncoder encoder= new BCryptPasswordEncoder();
 //        String encodedPassword= encoder.encode(users.getPassword());
 //        users.setPassword(encodedPassword);
@@ -105,45 +107,47 @@ public class AppController {
     }
 
     @RequestMapping("/edit/{product_id}")
-    public ModelAndView showNewEditForm(@PathVariable(name="product_id") int product_id){
-        ModelAndView mav= new ModelAndView("edit_product");
+    public ModelAndView showNewEditForm(@PathVariable(name = "product_id") int product_id) {
+        ModelAndView mav = new ModelAndView("edit_product");
 
-        Products product= service.get(product_id);
+        Products product = service.get(product_id);
         mav.addObject("product", product);
         return mav;
     }
 
     @RequestMapping("/edit_customer/{customer_id}")
-    public ModelAndView showNewEditCustomerForm(@PathVariable(name="customer_id") int customer_id){
-        ModelAndView mav= new ModelAndView("edit_customer");
+    public ModelAndView showNewEditCustomerForm(@PathVariable(name = "customer_id") int customer_id) {
+        ModelAndView mav = new ModelAndView("edit_customer");
 
-        Customers customers= customersService.get(customer_id);
+        Customers customers = customersService.get(customer_id);
         mav.addObject("customers", customers);
         return mav;
     }
+
     @RequestMapping("/editUserSR/{user_id}")
-    public ModelAndView showNewEditUserSRForm(@PathVariable(name="user_id") int user_id){
-        ModelAndView mav= new ModelAndView("editUserSR");
+    public ModelAndView showNewEditUserSRForm(@PathVariable(name = "user_id") int user_id) {
+        ModelAndView mav = new ModelAndView("editUserSR");
 
         //  Users users= usersService.get(user_id);
-        Users users= usersService.get(user_id);
+        Users users = usersService.get(user_id);
         mav.addObject("users", users);
         return mav;
     }
 
     @GetMapping("/delete/{product_id}")
-    public String deleteProduct(@PathVariable(name="product_id") Integer product_id){
+    public String deleteProduct(@PathVariable(name = "product_id") Integer product_id) {
         service.delete(product_id);
         return "redirect:/";
     }
 
     @GetMapping("/delete_customer/{customer_id}")
-    public String deleteCustomer(@PathVariable(name="customer_id") Integer customer_id){
+    public String deleteCustomer(@PathVariable(name = "customer_id") Integer customer_id) {
         customersService.delete(customer_id);
         return "redirect:/";
     }
+
     @GetMapping("/deleteUserSR/{user_id}")
-    public String deleteUserSR(@PathVariable(name="user_id") Integer user_id){
+    public String deleteUserSR(@PathVariable(name = "user_id") Integer user_id) {
         usersService.delete(user_id);
 
         return "redirect:/";
@@ -156,32 +160,17 @@ public class AppController {
 //    }
 
     @GetMapping("/403")
-    public String error403(){
+    public String error403() {
         return "403";
     }
 
 
     @GetMapping("/login")
-    public String showLoginPage(){
-        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
-        if(authentication==null || authentication instanceof AnonymousAuthenticationToken){
+    public String showLoginPage() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
             return "login";
         }
         return "redirect:/";
     }
-
-//    @GetMapping("/newSale")
-//    public String showNewSaleForm(Model model){
-//        List<Products>listProducts=service.listAll();
-//        model.addAttribute("sale", new Sale());
-//        model.addAttribute("listProducts", listProducts);
-//        return "newSale";
-//    }
-//
-//    @RequestMapping(value="/saveSale", method = RequestMethod.POST)
-//    public String saveSale(@ModelAttribute("sale") Sale sale){
-//        salesService.save(sale);
-//        return "redirect:/";
-//    }
-
 }
