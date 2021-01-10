0584aa4653b7b06a5d29a044b9af3414d506df8f
IvanovaIveta <73793415+IvanovaIveta@users.noreply.github.com>
2021-01-10 20:50:04 +0200

    Update UsersService.java

--- src/main/java/com/example/SalesManagerProducts/service/UsersService.java
+++ src/main/java/com/example/SalesManagerProducts/service/UsersService.java
@@ -1,26 +1,39 @@
 package com.example.SalesManagerProducts.service;
 
 
+import com.example.SalesManagerProducts.entities.Role;
 import com.example.SalesManagerProducts.entities.Users;
+import com.example.SalesManagerProducts.persistence.RoleRepository;
 import com.example.SalesManagerProducts.persistence.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
+
 @Service
-public class UsersService {
+public class UsersService extends UserDetailsServiceImpl {
 
     @Autowired
     private UserRepository repo;
 
-    public List<Users> listAll(){
-        return repo.findAll();
-    }
+    @Autowired
+    private RoleRepository roleRepository;
+
+
+
     public void save(Users users){
+        BCryptPasswordEncoder encoder= new BCryptPasswordEncoder();
+        String encodedPassword= encoder.encode(users.getPassword());
+        users.setPassword(encodedPassword);
+        Role role= roleRepository.findByName("SalesRepresentative" );
+        users.getRoles().add(role);
+        users.setEnabled(true);
         repo.save(users);
     }
+
     public Users get(Integer user_id){
         return repo.findById(user_id).get();
     }
@@ -29,9 +42,9 @@ public class UsersService {
     }
 
 
-     public List<Users> loadUserByRole() {
-
-         return repo.getUsersByRoles();
+    public List<Users> getRepresentatives(){
+        return repo.getUsersByRole("SalesRepresentative");
     }
+
 
 }
