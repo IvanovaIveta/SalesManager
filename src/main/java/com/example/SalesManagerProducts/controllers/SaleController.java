45ef389fc295eb5e11097bb56bfc7d33dcd454f1
IvanovaIveta <73793415+IvanovaIveta@users.noreply.github.com>
2021-01-10 20:32:57 +0200

    Update SaleController.java

--- src/main/java/com/example/SalesManagerProducts/controllers/SaleController.java
+++ src/main/java/com/example/SalesManagerProducts/controllers/SaleController.java
@@ -3,10 +3,17 @@ package com.example.SalesManagerProducts.controllers;
 import com.example.SalesManagerProducts.entities.Customers;
 import com.example.SalesManagerProducts.entities.Products;
 import com.example.SalesManagerProducts.entities.Sale;
+import com.example.SalesManagerProducts.entities.Users;
 import com.example.SalesManagerProducts.persistence.CustomersRepository;
 import com.example.SalesManagerProducts.persistence.ProductsRepository;
 import com.example.SalesManagerProducts.persistence.SalesRepository;
+import com.example.SalesManagerProducts.persistence.UserRepository;
+import com.example.SalesManagerProducts.service.ProductsService;
+import com.example.SalesManagerProducts.service.SalesService;
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +24,7 @@ import java.util.List;
 public class SaleController {
 
     @Autowired
-    private SalesRepository salesRepo;
+    private SalesService salesService;
 
     @Autowired
     private ProductsRepository productRepo;
@@ -25,23 +32,31 @@ public class SaleController {
     @Autowired
     private CustomersRepository customersRepo;
 
+    @Autowired
+    private UserRepository userRepo;
+
     @GetMapping("/newSale")
     public String showNewSaleForm(Model model){
         List<Products> listProducts=productRepo.findAll();
         model.addAttribute("sale", new Sale());
         model.addAttribute("listProducts", listProducts);
-
         List<Customers> listCustomers=customersRepo.findAll();
         model.addAttribute("listCustomers", listCustomers);
+//        Users user=request.getUserPrincipal().getName();
         return "newSale";
     }
 
 
     @PostMapping("/saveSale")
-    public String saveProduct(Sale sale){
-        salesRepo.save(sale);
-        return "sales";
+    public String saveSale(Sale sale){
+        salesService.save(sale);
+        return "redirect:/";
     }
 
+
+//    @RequestMapping(method = RequestMethod.POST)
+//    public String sellProduct(@RequestBody SaleModel sale, Authentication authentication){
+//         Users salesRepresentative= userRepo.getUserByUsername(get
+//    }
 
 }
