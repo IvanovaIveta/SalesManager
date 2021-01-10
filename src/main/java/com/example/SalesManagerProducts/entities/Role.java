71e04758b33ace97c1a67025d889b76f19d1bcdb
IvanovaIveta <73793415+IvanovaIveta@users.noreply.github.com>
2021-01-10 20:37:29 +0200

    Update Role.java

--- src/main/java/com/example/SalesManagerProducts/entities/Role.java
+++ src/main/java/com/example/SalesManagerProducts/entities/Role.java
@@ -1,15 +1,36 @@
 package com.example.SalesManagerProducts.entities;
 
+import org.hibernate.annotations.CollectionId;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name="roles")
 public class Role {
     @Id
-    @Column(name="roles_id")
+    @Column(name="roles_id", nullable=false, unique=true)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer roles_id;
+
+    @Column(name="name")
     private String name;
+
+//
+//    public Role() {
+//       super();
+//    }
+
+    @ManyToMany(mappedBy="roles")
+    private List<Users> users;
+
+    public Role(Integer roles_id) {
+        super();
+        this.roles_id = roles_id;
+    }
+    public Role(){
+
+    }
 
     public Integer getRoles_id() {
         return roles_id;
