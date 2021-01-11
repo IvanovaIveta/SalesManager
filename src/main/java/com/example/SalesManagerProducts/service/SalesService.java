3770761e9da82c6a4b651957e3adb3a23b85eed7
IvanovaIveta <73793415+IvanovaIveta@users.noreply.github.com>
2021-01-11 03:34:28 +0200

    Update SalesService.java

--- src/main/java/com/example/SalesManagerProducts/service/SalesService.java
+++ src/main/java/com/example/SalesManagerProducts/service/SalesService.java
@@ -1,12 +1,17 @@
 package com.example.SalesManagerProducts.service;
 
+import com.example.SalesManagerProducts.MyUserDetails;
 import com.example.SalesManagerProducts.entities.Products;
 import com.example.SalesManagerProducts.entities.Sale;
 import com.example.SalesManagerProducts.persistence.SalesRepository;
 import com.example.SalesManagerProducts.persistence.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,15 +21,30 @@ public class SalesService {
     private SalesRepository salesRepo;
 
 
-
+    private static String getUsername(){
+        SecurityContext securityContext= SecurityContextHolder.getContext();
+        Authentication authentication= securityContext.getAuthentication();
+        String username=null;
+        if(authentication!=null){
+            MyUserDetails myUserDetails= (MyUserDetails) authentication.getPrincipal();
+            username= myUserDetails.getUsername();
+        }
+        return username;
+    }
     Products product = new Products();
 
     public List<Sale> listAll(){
         return salesRepo.findAll();
     }
 
+//    public void save(Sale sale){
+//        salesRepo.save(sale);
+//    }
+
+    Date date= new Date(System.currentTimeMillis());
     public void save(Sale sale){
-        salesRepo.save(sale);
+        Sale sale1=new Sale(sale.getSale_id(),sale.getDateSold(),sale.getQuantity_sold(),sale.getProduct(),sale.getBuyer(),getUsername());
+        salesRepo.save(sale1);
     }
 
     public Sale get(Integer saleId){
